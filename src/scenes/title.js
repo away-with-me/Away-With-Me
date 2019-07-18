45e572329466247fe112aeba40d0d0cb84735bf9
juniper wilde <juniper.r.wilde@gmail.com>
2019-07-17 20:01:10 -0700

    menu state handling and placeholders

--- src/scenes/title.js
+++ src/scenes/title.js
@@ -1,26 +1,39 @@
 import {CANVAS_HEIGHT, CANVAS_WIDTH} from "../constants";
 
+const CURSOR_X = 40;
+const CURSOR_START_Y = 85;
+const CURSOR_CONTROLS_Y = 105;
+const CURSOR_CREDITS_Y = 125;
+
+const menuSelectedEnum = {
+  "Start": 0,
+  "Controls": 1,
+  "Credits": 2
+};
+
+let menuState = menuSelectedEnum.Start;
+
 const titleScene = {
   key: "title",
 
-  preload() {},
+
+  preload() {
+    this.load.image('main', 'title/main.png');
+    this.load.image('cursor', 'title/cursor.png');
+  },
 
   create() {
-    const title = this.add.text(
-      CANVAS_WIDTH / 2,
-      CANVAS_HEIGHT * 0.2,
-      "AWAY WITH ME!",
-      {
-        fontSize: 30
-      },
-    );
-    title.setOrigin(0.5, 0.5);
+    this.add.image(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 'main');
+    this.add.image(CURSOR_X, CURSOR_START_Y, 'cursor');
     this.cursorKeys = this.input.keyboard.createCursorKeys();
   },
 
   update() {
     if (this.cursorKeys.space.isDown) {
-      this.scene.switch("game");
+      console.log(menuState);
+      if (menuState === menuSelectedEnum.Start) {
+        this.scene.switch("game");
+      }
     }
   }
 };
